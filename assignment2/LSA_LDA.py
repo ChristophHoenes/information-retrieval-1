created evaluate function and fix kl div

assignment2/LSA_LDA.py
@@ -198,7 +198,7 @@ class LDA():
             with open(index_path, "rb") as reader:
                 index = pkl.load(reader)
         #sims = [(self.index2docid[i], kl_divergence(self.model[doc], vec_lda)) for i, doc in enumerate(index)]
-        sims = [kl_divergence(self.model[doc], vec_lda) for doc in index]
+        sims = [kl_divergence(self.model[doc], vec_lda, self.num_topics) for doc in index]
         sims = sorted(enumerate(sims), key=lambda item: -item[1])
         sims = [(self.index2docid[idx], np.float64(value)) for (idx, value) in sims]
         return sims

assignment2/utils.py
@@ -57,16 +57,16 @@ def bow2tfidf(bow_vec, d):
     return result
 
 
-def list_of_tuples2np_array(li):
-    result = np.zeros(len(li))
+def list_of_tuples2np_array(li, num_topics):
+    result = np.zeros(num_topics)
     for (idx, value) in li:
         result[idx] = value
     return result
 
 
-def kl_divergence(p, q):
-    p = list_of_tuples2np_array(p)
-    q = list_of_tuples2np_array(q)
+def kl_divergence(p, q, num_topics):
+    p = list_of_tuples2np_array(p, num_topics)
+    q = list_of_tuples2np_array(q, num_topics)
     sum_pq = np.sum(np.where(p != 0, p * np.log(p / q), 0))
     sum_qp = np.sum(np.where(q != 0, q * np.log(q / p), 0))
     return float((sum_pq + sum_qp) / 2)