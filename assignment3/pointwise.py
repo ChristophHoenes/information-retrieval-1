bad1b367a00f1ced0a40e5e2c7e5cc59610cf74e
ykerus <yke.rusticus@gmail.com>
2020-03-14 19:54:10 +0100

    pointwise finished

--- assignment3/pointwise.py
+++ assignment3/pointwise.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 
 class MLP(nn.Module):
     
-    #n_hidden is list with len num_layers 
+    #n_hiddens is list with len num_layers 
     #specifying layer width per layer
     # if list is empty, no hiddens
-    def __init__(self, n_inputs, n_hidden):
+    def __init__(self, n_inputs, n_hiddens):
         super(MLP, self).__init__()
         
-        if len(n_hidden) == 0:
+        if len(n_hiddens) == 0:
             linears = [nn.Linear(n_inputs, 1)]
         else:
-            linears = [nn.Linear(n_inputs, n_hidden[0])]
-            for l in range(len(n_hidden)-1):
-                linears.append(nn.Linear(n_hidden[l], n_hidden[l+1]))
-            linears.append(nn.Linear(n_hidden[-1], 1))
+            linears = [nn.Linear(n_inputs, n_hiddens[0])]
+            for l in range(len(n_hiddens)-1):
+                linears.append(nn.Linear(n_hiddens[l], n_hiddens[l+1]))
+            linears.append(nn.Linear(n_hiddens[-1], 1))
             
         self.linears = nn.ModuleList(linears)
         self.relu = nn.ReLU()
